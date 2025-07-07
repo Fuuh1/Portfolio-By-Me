8352c05b815a05459a4a522d5250c7c1ce4adc03
Yeee <abcu@example.com>
2025-07-07 11:57:56 +0530

    Skills Section Completed, with Css ..

---
+++ script.js
@@ -0,0 +1,14 @@
+// let bar = document.querySelectorAll(".skill-bar");
+// bar.forEach((bar) => {
+//   const percent = bar.querySelector(".percent").innerText.trim();
+//   const progress = bar.querySelector(".skill-progress");
+//   progress.style.width = percent;
+// });
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   document.querySelectorAll(".skill-bar").forEach((bar) => {
+//     const percentText = bar.querySelector(".percent").innerText.trim();
+//     const numeric = parseFloat(percentText); // strips the “%”
+//     bar.querySelector(".skill-progress").style.width = numeric + "%";
+//   });
+// });
